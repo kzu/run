3f56235f89d91e096636d051ffff58840ee66af3
Daniel Cazzulino <daniel@cazzulino.com>
2025-11-19 13:39:42 -0300

    Improve CLI docs, allow cancellation

--- clean.cs
+++ clean.cs
@@ -7,15 +7,29 @@
 using Spectre.Console;
 using ConsoleAppFramework;
 
-ConsoleApp.Run(args, (bool dryRun) => DeleteDirectories(Directory.GetCurrentDirectory(), dryRun));
+ConsoleApp.Run(args, Clean);
 
-static void DeleteDirectories(string dir, bool dryRun)
+/// <summary>Recursively cleans bin/obj</summary>
+/// <param name="dir">Optional directory to start the clean. Defaults to current directory.</param>
+/// <param name="dryRun">List directories but don't actually delete them.</param>
+static void Clean(bool dryRun, [Argument] string? dir = default, CancellationToken cancellation = default)
+    => DeleteDirectories(dir ?? Directory.GetCurrentDirectory(), dryRun, cancellation);
+
+static void DeleteDirectories(string dir, bool dryRun, CancellationToken cancellation)
 {
+    if (cancellation.IsCancellationRequested)
+        return;
+
     TryDeleteDirectory(Path.Combine(dir, "bin"), dryRun);
     TryDeleteDirectory(Path.Combine(dir, "obj"), dryRun);
 
     foreach (string subDir in Directory.GetDirectories(dir))
-        DeleteDirectories(subDir, dryRun);
+    {
+        if (cancellation.IsCancellationRequested)
+            break;
+            
+        DeleteDirectories(subDir, dryRun, cancellation);
+    }
 }
 
 static void TryDeleteDirectory(string dir, bool dryRun)
